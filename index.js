f3ad90ef05972ac1496f896b8568baf5418c8ea6
kaliyappan <141757796+Kaliyappan1@users.noreply.github.com>
2024-03-19 21:31:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ]
+});
+
+logger.info('hello');
+
+logger.error('Something went wrong', { 
+    error: new Error('something went wrong')
+});
