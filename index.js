b3fec4b27ac66f0923d97ad6a06868d40fe39524
kaliyappan <yappank17@gmail.com>
2024-03-27 21:19:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const winston = require('winston')
+const {combine, timestamp, json, prettyPrint } = winston.format
+
+/*const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  verbose: 4,
+  debug: 5,
+  silly: 6
+}; */
+
+const logger = winston.createLogger({
+    level: 'info',
+    
+    format: combine(
+        timestamp(),
+        json(),
+        prettyPrint()
+    ),
+    defaultMeta: {service: 'user-service'},
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({filename: 'error.log', level: 'error'}),
+        new winston.transports.File({filename: 'index.log'}),
+    ]
+});
+
+
+
+logger.log({
+    level:'error',
+    message: 'this comment is danger'
+})
+
+logger.info('success the logging')
+
+
+
