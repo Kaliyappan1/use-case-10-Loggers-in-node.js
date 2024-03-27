ef574dc654bef9e6ca7b3d7317c22f241a5810e3
kaliyappan <141757796+Kaliyappan1@users.noreply.github.com>
2024-03-27 21:27:00 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-const winston = require('winston');
+const winston = require('winston')
+const {combine, timestamp, json, prettyPrint } = winston.format
+
+/*const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  verbose: 4,
+  debug: 5,
+  silly: 6
+}; */
 
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ]
+    level: 'info',
+    
+    format: combine(
+        timestamp(),
+        json(),
+        prettyPrint()
+    ),
+    defaultMeta: {service: 'user-service'},
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({filename: 'error.log', level: 'error'}),
+        new winston.transports.File({filename: 'index.log'}),
+    ]
 });
 
-logger.info('hello');
 
-logger.error('Something went wrong', { 
-    error: new Error('something went wrong')
-});
+
+logger.error({
+    level:'error',
+    message: 'this comment is danger'
+})
+
+logger.info('success the logging')
+
+
+
